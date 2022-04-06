79ecba4b313c9d86f2d49db8e724d41c751894ca
klassiker <6598117+klassiker@users.noreply.github.com>
2022-04-06 22:04:27 +0200

    Add setup from gramineOS

--- impish-debug-env.dockerfile
+++ impish-debug-env.dockerfile
@@ -5,4 +5,18 @@ RUN apt-get update && xargs -a packages.txt -r apt-get install -y
 
 COPY debug-env/entrypoint.sh /
 
+WORKDIR /gramine-os/sgx-signer-key
+RUN openssl genrsa -3 -out /gramine-os/sgx-signer-key/enclaive-key.pem 3072
+ENV SGX_SIGNER_KEY /gramine-os/sgx-signer-key/enclaive-key.pem
+
+WORKDIR /manifest
+COPY scripts/manifest.sh .
+RUN chmod +x manifest.sh
+RUN echo "alias manifest='/manifest/manifest.sh'" >> ~/.bashrc
+
+WORKDIR /entrypoint
+COPY scripts/enclaive.sh .
+RUN chmod +x enclaive.sh
+RUN echo "alias enclaive='/entrypoint/enclaive.sh'" >> ~/.bashrc
+
 ENTRYPOINT [ "/entrypoint.sh" ]
---
+++ scripts/manifest.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+if [ -n "$1" ]; then
+  cd /manifest
+
+        test -f $1.manifest && rm *.sig *.token *.sgx *.manifest
+
+        gramine-manifest \
+        -Dlog_level=error \
+        -Darch_libdir=/lib/x86_64-linux-gnu \
+        $1.manifest.template $1.manifest
+
+        gramine-sgx-sign \
+            --key $SGX_SIGNER_KEY \
+            --manifest $1.manifest \
+            --output $1.manifest.sgx
+
+        gramine-sgx-get-token -s $1.sig -o $1.token     # requires $1.manifest.sgx
+
+        # clean up
+        rm -rf /gramine-os/sgx-signer-key
+
+else
+  echo "Manifest: argument missing"
+fi
